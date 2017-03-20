502035e00cf1d21498df274de5e48731e7d82a93
Mathieu Monteverde <mathieu.monteverde@heig-vd.ch>
2017-03-20 11:09:19 +0000

    Rendu du quiz RES

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
@@ -1,5 +1,9 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
 
 /**
  * 
@@ -14,6 +18,61 @@ public class Application {
   }
 
   IStreamDecoratorController getStreamDecoratorController() {
-    return null;
+    return new IStreamDecoratorController() {
+       @Override
+       public Reader decorateReader(Reader inputReader) {
+          return inputReader;
+       }
+
+       @Override
+       public Writer decorateWriter(Writer outputWriter) {
+          
+          return new Writer() {
+             private Writer wr = outputWriter;
+             
+             @Override
+             public void close() throws IOException {
+                wr.close();
+             }
+             
+             @Override
+             public void flush() throws IOException{
+                wr.flush();
+             }
+
+             @Override
+             public void write(char[] cbuf, int off, int len) throws IOException {
+                
+                // Create a new buffer
+                char[] filteredBuf = new char[cbuf.length];
+                int j = 0;
+                
+                // length used to correct the final length of the buffer
+                int finalLen = len;
+                
+                // Copy everything that is not an 'A' or an 'a'
+                for (int i = off; i < len; ++i) {
+                   if (cbuf[i] != 'a' && cbuf[i] != 'A') {
+                      filteredBuf[j] = cbuf[i];
+                      ++j;
+                   } else {
+                      finalLen--;
+                   }                
+                }
+                
+                wr.write(filteredBuf, off, finalLen);
+             }
+
+             @Override
+             public void write(int c) throws IOException {
+                System.out.println(c);
+                
+                if (c != 'a' && c != 'A')
+                   wr.write(c);
+             }
+          };
+          
+       }
+    };
   }
 }
--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
@@ -1,20 +1,43 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
 /**
  * This class is responsible for providing different types of Stream Processors.
- * 
+ *
  * @author Olivier Liechti
  */
 public class StreamProcessorsFactory implements IStreamProcessorsFactory {
 
-  @Override
-  public IStreamProcessor getProcessor() {
-    return new BasicStreamProcessor();
-  }
+   @Override
+   public IStreamProcessor getProcessor() {
+      return new BasicStreamProcessor();
+   }
 
-  @Override
-  public IStreamProcessor getProcessor(String processorName) throws UnknownNameException {
-    throw new UnknownNameException("The factory does not know any processor called " + processorName);
-  }
+   @Override
+   public IStreamProcessor getProcessor(String processorName) throws UnknownNameException {
+      if (processorName.equals("e-remover")) {
+         return new IStreamProcessor() {
+            @Override
+            public void process(Reader in, Writer out) throws IOException {
+               BufferedReader br = new BufferedReader(in);
+               BufferedWriter bw = new BufferedWriter(out);
+               int c = br.read();
+               while (c != -1) {
+                  if (c != 'e' && c != 'E')
+                     bw.write(c);
+                  c = br.read();
+               }
+               bw.flush();
+            }
+         };
+      } else {
+         return getProcessor();
+      }
+   }
 
 }
